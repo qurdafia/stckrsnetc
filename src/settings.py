27bacd6978e8b080f439a09d225e2eef8f586aa6
Mark Roma <fragrancegarden@gmail.com>
2021-03-27 14:19:59 +0800

    Adding secrets.

--- src/settings.py
+++ src/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ STATIC_DIR = os.path.join('static', BASE_DIR)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pxz!5e7vki$4%fx2+xb-z6v_85b)0j)nwontigspp#!+fpxu*2'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,12 +140,12 @@ LOGOUT_URL = 'logout'
 # EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 
 # Email Backend
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-pulse.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "contact@unrestph.com"
-EMAIL_HOST_PASSWORD = "dG8m5ACLS8"
+EMAIL_BACKEND = secrets.EMAIL_BACKEND
+EMAIL_HOST = secrets.EMAIL_HOST
+EMAIL_USE_TLS = secrets.EMAIL_USE_TLS
+EMAIL_PORT = secrets.EMAIL_PORT
+EMAIL_HOST_USER = secrets.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = secrets.EMAIL_HOST_PASSWORD
 
 # Images file
 MEDIA_URL = '/media/'
